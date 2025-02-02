00f53d7cb1d4720c9cfe530173c354975145ccf9
Umka1989 <pspiskunov@gmail.com>
2025-02-02 15:21:10 +0300

    Create Dashboard.js

---
+++ front/app/src/Dashboard.js
@@ -0,0 +1,15 @@
+import Header from "./common/Header";
+import Footer from "./common/Footer";
+
+
+function Dashboard (props){
+    return (
+       <div>
+           <Header />
+           <div>Dashboard</div>
+           <Footer />
+       </div>
+    )
+}
+
+export default Dashboard;
