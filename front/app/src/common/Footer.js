ea3166d60601ad9b663ce3ba4482f758f0ada5af
Umka1989 <pspiskunov@gmail.com>
2025-02-02 15:26:10 +0300

    Create Footer.js

---
+++ front/app/src/common/Footer.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+function Footer (props){
+    return (
+       <div className={styles.footer}>
+           <div>Footer</div>
+       </div>
+    )
+}
+
+export default Footer;
