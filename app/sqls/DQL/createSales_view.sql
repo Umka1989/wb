548a986e9e61dac53086f79e605996ca1a2dd568
k3214891 <109734968+k3214891@users.noreply.github.com>
2025-02-08 13:00:19 +0300

    Update createSales_view.sql
    добавил столбец "size_name"

--- app/sqls/DQL/createSales_view.sql
+++ app/sqls/DQL/createSales_view.sql
@@ -1,5 +1,5 @@
 CREATE or REPLACE VIEW sales
 AS
-select id , 'ozon' as table_name, ozon_article as article, product_name, customer_price_amt as amount,
+select id , 'ozon' as table_name, ozon_article as article, size_name, product_name, customer_price_amt as amount,
 quantity
 from sales_ozon
