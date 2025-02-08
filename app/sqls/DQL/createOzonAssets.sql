5bcb2971b95d8f77cb89fcd48b77323fc5af24f3
k3214891 <109734968+k3214891@users.noreply.github.com>
2025-02-08 11:52:55 +0300

    Update createOzonAssets.sql
    добавлен столбец "size_name"

--- app/sqls/DQL/createOzonAssets.sql
+++ app/sqls/DQL/createOzonAssets.sql
@@ -3,9 +3,10 @@ CREATE TABLE IF NOT EXISTS public.assets_ozon
 	id serial PRIMARY KEY,
     sku integer NOT NULL,
     feature varchar(100),
-    store_name varchar(250) ,
+    store_name varchar(250),
     cluster varchar(250) NOT NULL,
     article varchar(250) NOT NULL,
+	size_name varchar(100) NOT NULL,
     product_name varchar(250) NOT NULL,
     count integer NOT NULL,
     status varchar(35) NOT NULL
