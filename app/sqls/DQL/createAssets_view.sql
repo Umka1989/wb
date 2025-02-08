ea2ed7715d70f30eef9cac6b1894add17eec21bb
k3214891 <109734968+k3214891@users.noreply.github.com>
2025-02-08 12:40:30 +0300

    Update createAssets_view.sql
    добавил "size_name"

--- app/sqls/DQL/createAssets_view.sql
+++ app/sqls/DQL/createAssets_view.sql
@@ -1,7 +1,7 @@
 CREATE or REPLACE VIEW assets
 AS
-select id , 'ozon' as table_name, storage_name, article, product_name, avalible_to_sale as count_
+select id , 'ozon' as table_name, storage_name, article, size_name, product_name, avalible_to_sale as count_
 from assets_ozon
 union
-select id , 'wb' as table_name, storage as storage_name, article, type_name, total_by_storage as count_
+select id , 'wb' as table_name, storage as storage_name, article, size_name, type_name, total_by_storage as count_
 from assets_wb;
